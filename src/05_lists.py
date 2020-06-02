faefe15a031a199af0912159288f1f771a90b752
Shamskol <koladejun@yahoo.com>
2020-06-02 21:13:03 +0100

    Completed more exercices

--- src/05_lists.py
+++ src/05_lists.py
@@ -8,19 +8,22 @@ y = [8, 9, 10]
 
 # Change x so that it is [1, 2, 3, 4]
 # YOUR CODE HERE
-#print(x.append(4))
+print(x.append(4))
 
 # Using y, change x so that it is [1, 2, 3, 4, 8, 9, 10]
 # YOUR CODE HERE
-print(x + y)
+z = x + y
+print(z)
 
 # Change x so that it is [1, 2, 3, 4, 9, 10]
 # YOUR CODE HERE
-print(x)
+del z[4]
+print(z)
 
 # Change x so that it is [1, 2, 3, 4, 9, 99, 10]
 # YOUR CODE HERE
-print(x)
+z.insert(5, 99)
+print(z)
 
 # Print the length of list x
 
